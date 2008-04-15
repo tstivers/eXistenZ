48a5e5fd23f2253871acd6e15410bd0248b5f50a
tstivers <tstivers@localhost>
2008-04-15 09:30:53 +0000

    * added native function calls for timer events

--- engine/timer/timers.cpp
+++ engine/timer/timers.cpp
@@ -1,6 +1,7 @@
 #include "precompiled.h"
 #include "timer/timer.h"
 #include "timer/timers.h"
+#include "timer/jstimers.h"
 #include "console/console.h"
 
 namespace timer {
@@ -8,10 +9,14 @@ namespace timer {
 	public:
 		Timer(const string& name, const string& action, const unsigned int frequency_ms, const unsigned int next_ms) :
 		  name(name), action(action), frequency_ms(frequency_ms), next_ms(next_ms){};
+		Timer(const string& name, function<void(string)> action, const unsigned int frequency_ms, const unsigned int next_ms) :
+		  name(name), action_f(action), frequency_ms(frequency_ms), next_ms(next_ms){};
+
 		~Timer() {};
 		
 		string name;
 		string action;
+		function<void(string)> action_f;
 		float frequency_ms;
 		float next_ms;		
 	};
@@ -27,30 +32,37 @@ namespace timer {
 	};
 
 	typedef priority_queue<pTimer, deque<pTimer>, timer_less> timerqueue_t;
-	typedef stdext::hash_map<string, pTimer> timermap_t;
+	typedef unordered_map<string, pTimer> timermap_t;
 	timerqueue_t timer_queue;
 	timermap_t timer_map;
+
+	void addTimer(pTimer timer);
 }
 
 using namespace timer;
 
 bool timer::addTimer(const string& name, const string& action, float frequency_ms /* = 0 */, float next_ms /* = 0 */)
 {
-	ASSERT(frequency_ms >= 0.0f);
-
-	timermap_t::iterator i = timer_map.find(name);
-	if(i != timer_map.end()) {
-		removeTimer(i->second->name);
-	}
-
-	if(next_ms < game_ms)
-		next_ms = game_ms;
-
-	pTimer t(new Timer(name, action, frequency_ms, next_ms));
-	timer_map.insert(timermap_t::value_type(name, t));
-	timer_queue.push(t);
-
+	addTimer(pTimer(new Timer(name, action, frequency_ms, next_ms)));
 	return true;
+}
+
+bool timer::addTimer(const string& name, function<void(string)> action, float frequency_ms /* = 0 */, float next_ms /* = 0 */)
+{
+	addTimer(pTimer(new Timer(name, action, frequency_ms, next_ms)));
+	return true;
+}
+
+void timer::addTimer(pTimer timer)
+{
+	ASSERT(timer->frequency_ms >= 0.0f);
+	if(timer->next_ms < game_ms)
+		timer->next_ms = game_ms;
+
+	removeTimer(timer->name);
+
+	timer_map.insert(timermap_t::value_type(timer->name, timer));
+	timer_queue.push(timer);
 }
 
 bool timer::removeTimer(const string& name)
@@ -71,6 +83,7 @@ bool timer::removeTimer(const string& name)
 		timer_queue.push(*j);
 
 	timer_map.erase(i);
+	jstimer::removeTimerCallback(name);
 		
 	return false;
 }
@@ -81,7 +94,10 @@ void timer::fireTimers()
 	{
 		pTimer t = timer_queue.top();
 		timer_queue.pop();
-		console::processCmd(t->action.c_str());
+		if(t->action_f)
+			t->action_f(t->name);
+		else
+			console::processCmd(t->action.c_str());
 		if(t->frequency_ms != 0.0f) {
 			t->next_ms += t->frequency_ms;
 			if(t->next_ms <= game_ms)
@@ -89,6 +105,7 @@ void timer::fireTimers()
 			timer_queue.push(t);
 		} else {			
 			timer_map.erase(t->name);
+			jstimer::removeTimerCallback(t->name);
 		}
 	}
 }
