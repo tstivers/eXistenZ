c795382cdd50eee390b9555153f462da7d0d6a66
tstivers <tstivers@localhost>
2008-06-18 00:19:08 +0000

    * added lazy resolution of entities to the entity manager

--- engine/entity/jsentitymanager.h
+++ engine/entity/jsentitymanager.h
@@ -6,7 +6,6 @@ namespace jsentity
 {
 	JSObject* createEntityManager(entity::EntityManager* manager);
 	void destroyEntityManager(entity::EntityManager* manager);
-	JSBool enumerateEntities(JSContext *cx, JSObject *obj, JSIterateOp enum_op, jsval *statep, jsid *idp);
 
 	extern JSClass manager_class;
 	extern JSObject* manager_prototype;
