701b0e9f573493b95c8c9ade239a948d40b25658
tstivers <tstivers@localhost>
2008-06-21 16:42:12 +0000

    * starting javascript object cleanup

--- engine/entity/jscomponent.cpp
+++ engine/entity/jscomponent.cpp
@@ -2,43 +2,19 @@
 #include "entity/jscomponent.h"
 #include "entity/component.h"
 
-using namespace jsentity;
 using namespace entity;
+using namespace script;
+using namespace jsentity;
 
 namespace jsentity
 {
-	static void initComponentClass(ScriptEngine* engine);
-	static entity::Component* getComponentReserved(JSContext* cx, JSObject* obj);
+	// property declarations
+	// static JSBool propGetter/propSetter(JSContext *cx, JSObject *obj, jsval id, jsval *vp);
 
 	// method declarations
-	// static JSBool classMethod(JSContext *cx, uintN argc, jsval *vp);
-	static JSBool acquire(JSContext* cx, uintN argc, jsval* vp);
-	static JSBool release(JSContext* cx, uintN argc, jsval* vp);
+	// static JSBool methodName(JSContext *cx, uintN argc, jsval *vp);
 
-	// property implementations
-	static JSBool name_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp);
-	static JSBool type_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp);
-	static JSBool typeName_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp);
-
-	JSObject* component_prototype = NULL;
-
-	JSFunctionSpec component_methods[] =
-	{
-		//JS_FN("removeComponent", removeComponent, 1, 1, 0),
-		JS_FN("acquire", acquire, 0, 0, 0),
-		JS_FN("release", release, 0, 0, 0),
-		JS_FS_END
-	};
-
-	JSPropertySpec component_props[] =
-	{
-		{"name", 1, JSPROP_PERMANENT | JSPROP_SHARED | JSPROP_READONLY, name_getter, NULL},
-		{"type", 1, JSPROP_PERMANENT | JSPROP_SHARED | JSPROP_READONLY, type_getter, NULL},
-		{"typeName", 1, JSPROP_PERMANENT | JSPROP_SHARED | JSPROP_READONLY, typeName_getter, NULL},
-		JS_PS_END 
-	};
-
-	JSClass component_class =
+	static JSClass class_def =
 	{
 		"Component",
 		JSCLASS_HAS_RESERVED_SLOTS(1),
@@ -47,75 +23,49 @@ namespace jsentity
 		JS_EnumerateStub, JS_ResolveStub,
 		JS_ConvertStub,  JS_FinalizeStub
 	};
+
+	static JSPropertySpec class_properties[] =
+	{
+		// {"name", id, flags, getter, setter},
+		{"name", 1, JSPROP_PERMANENT | JSPROP_SHARED | JSPROP_READONLY, PropertyGetter<Component, const string&, &Component::getName>, NULL},
+		{"type", 1, JSPROP_PERMANENT | JSPROP_SHARED | JSPROP_READONLY, PropertyGetter<Component, int, &Component::getType>, NULL},
+		{"typeName", 1, JSPROP_PERMANENT | JSPROP_SHARED | JSPROP_READONLY, PropertyGetter<Component, const string&, &Component::getTypeName>, NULL},
+		JS_PS_END
+	};
+
+	static JSFunctionSpec class_methods[] =
+	{
+		// JS_FN("name", function, nargs, flags, minargs),
+		JS_FN("acquire", AcquireObject<Component>, 0, 0, 0),
+		JS_FN("release", ReleaseObject<Component>, 0, 0, 0),
+		JS_FS_END
+	};
 }
 
-REGISTER_SCRIPT_INIT(Component, initComponentClass, 15);
-
-void jsentity::initComponentClass(ScriptEngine* engine)
+ScriptedObject::ScriptClass Component::m_scriptClass =
 {
-	component_prototype = JS_InitClass(
-		engine->GetContext(),
-		engine->GetGlobal(),
-		NULL,
-		&component_class,
-		NULL,
-		0,
-		component_props,
-		component_methods,
-		NULL,
-		NULL);
+	&class_def,
+	class_properties,
+	class_methods,
+	NULL
+};
 
-	ASSERT(component_prototype);
-}
-
-entity::Component* jsentity::getComponentReserved(JSContext* cx, JSObject* obj)
+static void initClass(ScriptEngine* engine)
 {
-	ASSERT(obj != component_prototype);
-	jsval component = JSVAL_VOID;
-	JSBool ret = JS_GetReservedSlot(cx, obj, 0, &component);
-	ASSERT(ret == JS_TRUE);
-	ASSERT(component != JSVAL_VOID);
-	ASSERT(JSVAL_TO_PRIVATE(component) != NULL);
-	return (entity::Component*)JSVAL_TO_PRIVATE(component);
+	Component::m_scriptClass.prototype =
+		JS_InitClass(
+			engine->GetContext(),
+			engine->GetGlobal(),
+			NULL,
+			Component::m_scriptClass.classDef,
+			NULL,
+			0,
+			Component::m_scriptClass.properties,
+			Component::m_scriptClass.methods,
+			NULL,
+			NULL);
+
+	ASSERT(Component::m_scriptClass.prototype);
 }
 
-JSBool jsentity::name_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
-{
-	Component* component = getComponentReserved(cx, obj);
-	*vp = STRING_TO_JSVAL(JS_NewStringCopyZ(cx, component->getName().c_str()));
-	return JS_TRUE;
-}
-
-JSBool jsentity::type_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
-{
-	Component* component = getComponentReserved(cx, obj);
-	*vp = INT_TO_JSVAL(component->getType());
-	return JS_TRUE;
-}
-
-JSBool jsentity::typeName_getter(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
-{
-	Component* component = getComponentReserved(cx, obj);
-	*vp = STRING_TO_JSVAL(JS_NewStringCopyZ(cx, component->getTypeName().c_str()));
-	return JS_TRUE;
-}
-
-JSBool jsentity::acquire(JSContext *cx, uintN argc, jsval *vp)
-{
-	Component* c = GetReserved<Component>(cx, JS_THIS_OBJECT(cx, vp));
-
-	c->acquire();
-
-	JS_SET_RVAL(cx, vp, JSVAL_VOID);
-	return JS_TRUE;
-}
-
-JSBool jsentity::release(JSContext *cx, uintN argc, jsval *vp)
-{
-	Component* c = GetReserved<Component>(cx, JS_THIS_OBJECT(cx, vp));
-
-	c->release();
-
-	JS_SET_RVAL(cx, vp, JSVAL_VOID);
-	return JS_TRUE;
-}
+REGISTER_SCRIPT_INIT(Component, initClass, 15);
