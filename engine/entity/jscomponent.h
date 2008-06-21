9b37e97e06d5685b699de44fbf339fe47031f271
tstivers <tstivers@localhost>
2008-06-21 16:42:12 +0000

    * starting javascript object cleanup

--- engine/entity/jscomponent.h
+++ engine/entity/jscomponent.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include "entity/component.h"
-
 namespace jsentity
 {
-	void destroyComponentObject(entity::Entity* entity);
-
-	extern JSClass component_class;
-	extern JSObject* component_prototype;
-};
+}
